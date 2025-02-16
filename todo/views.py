869d97bebae004e924acfaf161c7cb606ecd5731
Olle Tidblom <tidblom.ollegmail.com>
2025-02-16 15:47:29 +0100

    Fixed userspecific todo lists

--- todo/views.py
+++ todo/views.py
@@ -16,26 +16,26 @@ class taskList(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
     
     def get_context_data(self, **kwargs):
-        return super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
+        context['count'] = context['tasks'].filter(completed=False).count()
         
         return context
 
 
-class taskDetail(DetailView):
+class taskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'todo/task.html'
     
-class createTask(CreateView):
+class createTask(LoginRequiredMixin, CreateView):
     model = Task 
     fields = '__all__'
     success_url = reverse_lazy('tasks')
     context_object_name = 'create_task'
     template_name = 'todo/create.html'
     
-class editTask(UpdateView):
+class editTask(LoginRequiredMixin, UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
@@ -43,7 +43,7 @@ class editTask(UpdateView):
     template_name = 'todo/edit.html'
     
     
-class deleteTask(DeleteView):
+class deleteTask(LoginRequiredMixin, DeleteView):
     model = Task 
     context_object_name ='task'
     template_name = 'todo/delete_task.html'
