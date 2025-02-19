c81e73a59a3bd6719c7b1d4d21366cd5a60ffe30
Olle Tidblom <tidblom.ollegmail.com>
2025-02-19 02:13:36 +0100

    added gunicorn and made ready for deployment to heruko

--- doit/settings.py
+++ doit/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -155,15 +155,7 @@ USE_TZ = True
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-""" 
-ACCOUNT_LOGIN_METHODS = {'email', 'username'}
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_USERNAME_REQUIRED = True
-ACCOUNT_PASSWORD_REQUIRED = True
-ACCOUNT_USERNAME_MIN_LENGTH = 4
-LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/'
-"""
+
 
 
 # Static files (CSS, JavaScript, Images)
